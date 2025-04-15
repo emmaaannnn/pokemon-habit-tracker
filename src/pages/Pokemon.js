f6e2c4ac5c5c72f61ce292a7d8e78c426fa3aa07
Emmanuel <emmanuel.almonte@students.mq.edu.au>
2025-04-15 20:20:19 +1000

    Login Page Css complete

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header.js';
 import Navbar from '../components/NavBar.js';
+import '../styles/Pokemon.css';
 
 const Pokemon = ({ setCurrentPage }) => {
   return (
